5b6b21bd2e5372fa0ff8260c6e64a2a21b2c30d1
Oona Carbonie <carbonie.oona@gmail.com>
2019-04-17 13:45:48 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+      public static void main(String[] args) {
+        System.out.print("Cost\t\tQuantity\tTotal");
+        System.out.print("\n$1,000.00" + "\t\t4" + "\t\t$4,000.00");
+        System.out.print("\n$\t50.00" + " " + "\t\t8" + "\t\t$" + " " + " 400.00");
+        System.out.print("\n_________________________________");
+        System.out.println("\nTotal" + "\t\t\t12" + "\t\t$4,400.00");
+    }
+}
